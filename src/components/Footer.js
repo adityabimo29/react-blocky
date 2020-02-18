5303b5c430bd7c761922c8b65d16e6a9603b9d94
adityabimo29 <porusruby@gmail.com>
2020-02-18 15:36:49 +0700

    fix

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 export default function Footer() {
     return (
-        <div className='footer py-2 mt-4'>
+        <div className='footer py-2'>
             <h3 className='text-white text-center pt-2'>Build With React + Express.</h3>
         </div>
     )
